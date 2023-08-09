56cfb3485886bd083f48c078060668b55cc6b3ee
ronald sarmiento <ronisarmont@gmail.com>
2023-08-08 20:03:14 -0400

    DatapoolConexions implemented part 2 prueba de conexiones abiertas

--- src/main/java/com/alura/jdbc/pruebas/PruebaPoolDeConexiones.java
+++ src/main/java/com/alura/jdbc/pruebas/PruebaPoolDeConexiones.java
@@ -1,4 +1,19 @@
 package com.alura.jdbc.pruebas;
 
+import com.alura.jdbc.factory.ConectionFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public class PruebaPoolDeConexiones {
+    public static void main(String[] args) throws SQLException {
+        ConectionFactory conectionFactory = new ConectionFactory();
+
+        for (int i = 0; i < 20; i++) {
+            Connection conexion = conectionFactory.recuperaConexion();
+
+            System.out.println("Abriendo la conexión número :"+(i+1));
+
+        }
+    }
 }
